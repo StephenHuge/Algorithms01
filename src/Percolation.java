b83d6b3357bd14f20fb23309b02b60f99133359e
HJS <407410014@qq.com>
2017-09-29 22:44:58 +0800

    finish method open()

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,18 +1,66 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-
+/**
+ * 实现一个Percolation类
+ * 
+ * @author HJS
+ * 
+ * @date 2017年9月29日
+ * 
+ */
 public class Percolation {
+    
+    private static final boolean OPENED = true;
+    
+    private static final boolean NOT_OPENED = false;
+    
+    private boolean[][] grid;
+    
+    private WeightedQuickUnionUF w;
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
         if(n <= 0) throw new java.lang.IllegalArgumentException();
+        grid = new boolean[n][n];
+        w = new WeightedQuickUnionUF(n * n);
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                grid[i][j] = false;
+            }
+        }
+        
     }
     // open site (row, col) if it is not open already
-    public void open(int row, int col) {}   
+    public void open(int row, int col) {
+        if(row < 0 || row > grid[0].length - 1 || col < 0 || col > grid.length - 1) 
+            throw new java.lang.IllegalArgumentException();
+        if(!isOpen(grid[row][col])) grid[row][col] = OPENED;
+
+        int position = row * grid.length + col;
+        int up = (row - 1) * grid.length + col;
+        int down = (row + 1) * grid.length + col;
+        int left = row * grid.length + (col - 1);
+        int right = row * grid.length + (col + 1);
+        
+        if(row != 0 && isOpen(grid[row - 1][col]))           w.union(position, up);
+        if((row != grid.length - 1) && isOpen(grid[row + 1][col])) w.union(position, down);
+        if(col != 0 && isOpen(grid[row][col - 1]))           w.union(position, left);
+        if((col != grid.length - 1) && isOpen(grid[row][col + 1])) w.union(position, right);
+    }
+    
+    private boolean isOpen(boolean g) {
+        return g;
+    }
+    
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        if(row < 0 || row > grid[0].length -1 || col < 0 || col > grid.length - 1) 
+            throw new java.lang.IllegalArgumentException();
+        return grid[row][col];
     } 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
+        if(row < 0 || row > grid[0].length - 1 || col < 0 || col > grid.length - 1) 
+            throw new java.lang.IllegalArgumentException();
         return false;
     } 
     public int numberOfOpenSites() {
@@ -23,5 +71,13 @@ public class Percolation {
         return false;
     }             
     // test client (optional)
-    public static void main(String[] args) {} 
+    public static void main(String[] args) {
+        Percolation p = new Percolation(10);
+        for(int i = 0; i < p.grid.length; i++) {
+            for(int j = 0; j < p.grid.length; j++) {
+                p.open(i, j);
+                System.out.println(i + " - " + j + ": " + p.grid[i][j]);
+            }
+        }
+    } 
 }
