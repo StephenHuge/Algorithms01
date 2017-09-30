1a40d284588c41a9d9336d63032c82abd76192f9
HJS - L <40741004@qq.com>
2017-09-30 23:18:02 +0800

    update comments

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -10,27 +10,43 @@ public class PercolationStats {
     
     private Percolation p;
     
-    // perform trials independent experiments on an n-by-n grid
+    private int sum;	// count of nodes that percolates in various experiments
+    
+    private int avg;	// equlas to sum / trials , get average nodes count of incident : percolate
+    
+    /*
+     * perform trials independent experiments on an n-by-n grid
+     */
     public PercolationStats(int n, int trials) {
         if(n <= 0 || trials <= 0) throw new java.lang.IllegalArgumentException();
             p = new Percolation(n);
     }
-    // sample mean of percolation threshold
+    /*
+     * sample mean of percolation threshold
+     */
     public double mean() {
         return -1;
     } 
-    // sample standard deviation of percolation threshold
+    /*
+     * sample standard deviation of percolation threshold
+     */
     public double stddev() {
         return -1;
     }
-    // low  endpoint of 95% confidence interval
+    /*
+     * low  endpoint of 95% confidence interval
+     */
     public double confidenceLo() {
         return -1;
     }
-    // high endpoint of 95% confidence interval
+    /*
+     * high endpoint of 95% confidence interval
+     */
     public double confidenceHi() {
         return -1;
     }  
-    // test client (described below)
+    /*
+     * test client (described below)
+     */
     public static void main(String[] args) {}  
 }
