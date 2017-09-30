84d33e092e2f258a23af7bc578196f880d249256
coder-god <407410014@qq.com>
2017-09-30 22:05:41 +0800

    safe point update

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,8 +1,19 @@
 
+/**
+ * 多次测试获取结果
+ * 
+ * @author 40741
+ *
+ * @date 2017年9月30日
+ */
 public class PercolationStats {
+    
+    private Percolation p;
+    
     // perform trials independent experiments on an n-by-n grid
     public PercolationStats(int n, int trials) {
         if(n <= 0 || trials <= 0) throw new java.lang.IllegalArgumentException();
+            p = new Percolation(n);
     }
     // sample mean of percolation threshold
     public double mean() {
