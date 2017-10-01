4ff47ae889d79c453596cbf83883fab9c8975229
pomony <40741004@qq.com>
2017-10-01 10:25:50 +0800

    change to use standard library and update the structure of constructor method

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdStats;
 
 /**
  * 多次测试获取结果
@@ -10,12 +11,14 @@ public class PercolationStats {
 
 	private Percolation p;
 
-	//    private int sum;	// count of nodes that percolates in various experiments
+	/**
+	 * array storing all the openSites in experiments
+	 */
+	private int[] openSites;
 	/*
 	 * equlas to sum / trials , get average nodes count of percolate incident
 	 */
 	private double avgSum;   
-
 	/*
 	 *  the sample standard deviation which measures the sharpness of the threshold
 	 */
@@ -32,24 +35,41 @@ public class PercolationStats {
 		if(n <= 0 || trials <= 0) throw new java.lang.IllegalArgumentException();
 		this.trials = trials;
 		
-		for (int i = 0; i < trials; i++) {    // for trials times experiments 
+		openSites = new int[trials];
+		calculate(n, trials);
+		log();
+	}
+
+    /**
+     * calculate the answer
+     */
+    private void calculate(int n, int trials) {
+        for (int i = 0; i < trials; i++) {    // for trials times experiments 
 		    p = new Percolation(n);
-		    
 		    int random, row, col, prclteTimes = 0;  
 		    for (int j = 0; j < n * n; j++) { // start n * n times open until this experiment percolates
-		        random = (int) (Math.random() * (n * n - 1));    // get a random number to open 
-//		        System.out.println("random: " + random);
-		        row = random / 10 + 1;    col = random % 10 + 1;
+		        random = (int) (Math.random() * (n * n - 1) + 1);    // get a random number to open, range is [1, n * n] 
+		        col = random % n;     
+		        row = col == 0 ? random / n : random / n + 1;
+		        col = col == 0 ? n : col;
 		        p.open(row, col);
-		        prclteTimes++;
-		        if(p.percolates())  break;
+		        if(p.percolates())  {     // this model percolates 
+		            prclteTimes = p.numberOfOpenSites();
+		            openSites[i] = prclteTimes;   // record openSites of this model
+		            break;   
+		        }
             }
-		    avgSum += prclteTimes;
-		    System.out.println("times of open operation before percolate: " + prclteTimes);
         }
-		avgSum = avgSum / trials;
-		System.out.println("Average times open operation before percolate: " + avgSum);
+    }
+	
+	private void log() {
+	    System.out.println("mean\t\t\t = " + mean());
+        System.out.println("stddev\t\t\t = " + stddev() );
+        System.out.println("95% confidence interval\t = [" + confidenceLo() + ", " + confidenceHi() + "]");
 	}
+	/**
+	 * get experiment times 
+	 */
 	private int getTrials() {
 	    return trials;
 	}
@@ -57,12 +77,14 @@ public class PercolationStats {
 	 * sample mean of percolation threshold
 	 */
 	public double mean() {
+	    avgSum = StdStats.mean(openSites) / getTrials();
 		return avgSum;
 	} 
 	/*
 	 * sample standard deviation of percolation threshold
 	 */
 	public double stddev() {
+	    stdDeviation = StdStats.stddev(openSites);
 		return stdDeviation;
 	}
 	/*
@@ -83,9 +105,8 @@ public class PercolationStats {
 	 * test client (described below)
 	 */
 	public static void main(String[] args) {
-	    int n = 10;
+	    int n = 200;
 	    int trials = 100;
 	    PercolationStats ps = new PercolationStats(n, trials);
-	    System.out.println("test: " + n + " by " + n + " grid, for " + trials + " times.");
 	}  
 }
