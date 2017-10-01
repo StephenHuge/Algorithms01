d09a979176d3a750c169db5461a41e428996c481
pomony <40741004@qq.com>
2017-10-01 13:36:47 +0800

    fix warnings and error

--- src/Percolation.java
+++ src/Percolation.java
@@ -17,19 +17,19 @@ public class Percolation {
     
     private int openedNum = 0;
     
-    private int virtualUpRoot;
-    private int virtualDownRoot;
+    private final int virtualUpRoot;
+    private final int virtualDownRoot;
     
-    private WeightedQuickUnionUF w;
+    private final WeightedQuickUnionUF w;
     /*
      * create n-by-n grid, with all sites blocked
      */
     public Percolation(int n) {
-        if(n <= 0) throw new java.lang.IllegalArgumentException();
+        if (n <= 0) throw new java.lang.IllegalArgumentException();
         
         grid = new boolean[n][n];
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 grid[i][j] = false;
             }
         }
@@ -42,9 +42,8 @@ public class Percolation {
      *  open site (row, col) if it is not open already
      */
     public void open(int row, int col) {
-        if(row < 1 || row > grid[0].length || col < 1 || col > grid.length) 
-            throw new java.lang.IllegalArgumentException();
-        if(!isOpen(row, col)) {
+        validate(row, col);
+        if (!isOpen(row, col)) {
             grid[row - 1][col - 1] = OPENED;
             openedNum++;
         }
@@ -55,17 +54,17 @@ public class Percolation {
         int left = (row - 1) * grid.length + (col - 2);
         int right = (row - 1) * grid.length + col;
         
-        if(row == 1)             // there is a virtual root for row 1, for simplify the codes
+        if (row == 1)             // there is a virtual root for row 1, for simplify the codes
             w.union(position, virtualUpRoot);
-        else if(row == grid.length)                      
+        else if (row == grid.length)                      
             w.union(position, virtualDownRoot);
-        if(row != 1 && isOpen(row - 1, col))    // whether in the up edge             
+        if (row != 1 && isOpen(row - 1, col))    // whether in the up edge             
             w.union(position, up);   
-        if((row != grid.length) && isOpen(row + 1, col)) // whether in the down edge 
+        if ((row != grid.length) && isOpen(row + 1, col)) // whether in the down edge 
             w.union(position, down);
-        if(col != 1 && isOpen(row, col - 1)) // whether in the left edge             
+        if (col != 1 && isOpen(row, col - 1)) // whether in the left edge             
             w.union(position, left);
-        if((col != grid.length) && isOpen(row, col + 1)) // whether in the right edge 
+        if ((col != grid.length) && isOpen(row, col + 1)) // whether in the right edge 
             w.union(position, right);
     }
     
@@ -73,22 +72,24 @@ public class Percolation {
      * is site (row, col) open?
      */
     public boolean isOpen(int row, int col) {
-        if(row < 1 || row > grid[0].length || col < 1 || col > grid.length) 
-            throw new java.lang.IllegalArgumentException();
+       validate(row, col);
         return grid[row - 1][col - 1];
     } 
     /*
      * is site (row, col) full (means that site gets water)?
      */
     public boolean isFull(int row, int col) {
-        if(row < 1 || row > grid[0].length || col < 1 || col > grid.length) 
-            throw new java.lang.IllegalArgumentException();
-        if(isOpen(row, col) && row == 1)    return true;    // 最上面一行默认有水
+        validate(row, col);
+        if (isOpen(row, col) && row == 1)    return true;    // 最上面一行默认有水
         int position = (row - 1) * grid.length + col - 1;   // 当前节点
-        if(w.connected(position, virtualUpRoot))            // 如果跟Up虚结点有连接则返回true   
+        if (w.connected(position, virtualUpRoot))            // 如果跟Up虚结点有连接则返回true   
             return true;
         return false;
     } 
+    private void validate(int row, int col) {
+        if (row < 1 || row > grid[0].length || col < 1 || col > grid.length) 
+            throw new java.lang.IllegalArgumentException();
+    }
     public int numberOfOpenSites() {
         return openedNum;
     }     
@@ -104,7 +105,7 @@ public class Percolation {
     public static void main(String[] args) {
         Percolation p = new Percolation(10);
         
-        for(int i = 1; i <= p.grid.length; i++) {
+        for (int i = 1; i <= p.grid.length; i++) {
             for(int j = 1; j <= p.grid.length; j++) {
                 p.open(i, j);
                 System.out.println(i + " - " + j + ": " + p.grid[i - 1][j - 1]);
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 /**
@@ -9,55 +10,44 @@ import edu.princeton.cs.algs4.StdStats;
  */
 public class PercolationStats {
 
-	private Percolation p;
-
 	/**
 	 * array storing all the openSites in experiments
 	 */
 	private int[] openSites;
-	/*
-	 * equlas to sum / trials , get average nodes count of percolate incident
-	 */
-	private double avgSum;   
-	/*
-	 *  the sample standard deviation which measures the sharpness of the threshold
-	 */
-	private double stdDeviation;	 
-
 	/**
 	 * the number n
 	 */
-	private int range;
+	private final int range;
 	/*
 	 *  experiment times
 	 */
-	private int trials;
+	private final int trials;
 	/*
 	 * perform trials independent experiments on an n-by-n grid
 	 */
 	public PercolationStats(int n, int trials) {
-		if(n <= 0 || trials <= 0) throw new java.lang.IllegalArgumentException();
+		if (n <= 0 || trials <= 0) throw new java.lang.IllegalArgumentException();
 		this.range = n;
 		this.trials = trials;
 		openSites = new int[trials];
 		calculate(n, trials);
-		log();
 	}
 
     /**
      * calculate the answer
      */
     private void calculate(int n, int trials) {
+        Percolation p;
         for (int i = 0; i < trials; i++) {    // for trials times experiments 
-		    p = new Percolation(n);
+            p = new Percolation(n);
 		    int random, row, col, prclteTimes = 0;  
 		    for (int j = 0; j < getPower2(n); j++) { // start n * n times open until this experiment percolates
-		        random = (int) (Math.random() * (getPower2(n) - 1) + 1);    // get a random number to open, range is [1, n * n] 
+		        random = StdRandom.uniform(getPower2(n) - 1) + 1;    // get a random number to open, range is [1, n * n] 
 		        col = random % n;     
 		        row = col == 0 ? random / n : random / n + 1;
 		        col = col == 0 ? n : col;
 		        p.open(row, col);
-		        if(p.percolates())  {     // this model percolates 
+		        if (p.percolates())  {     // this model percolates 
 		            prclteTimes = p.numberOfOpenSites();
 		            openSites[i] = prclteTimes;   // record openSites of this model
 		            break;   
@@ -66,12 +56,12 @@ public class PercolationStats {
         }
     }
 	
-	private void log() {
-	    System.out.println("mean\t\t\t = " + mean());
-        System.out.println("stddev\t\t\t = " + stddev() );
-        System.out.println("95% confidence interval\t = [" + confidenceLo() + ", " + confidenceHi() + "]");
+	private static void log(PercolationStats ps) {
+	    System.out.println("mean\t\t\t = " + ps.mean());
+        System.out.println("stddev\t\t\t = " + ps.stddev() );
+        System.out.println("95% confidence interval\t = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
 	}
-	private double getPower2(int num) {
+	private int getPower2(int num) {
 	    return num * num;
 	}
 	/**
@@ -87,7 +77,7 @@ public class PercolationStats {
 	 * sample mean of percolation threshold
 	 */
 	public double mean() {
-	    avgSum = StdStats.mean(openSites) / getPower2(getRange());
+	    double avgSum = StdStats.mean(openSites) / getPower2(getRange());
 		return avgSum;
 	} 
 
@@ -95,21 +85,22 @@ public class PercolationStats {
 	 * sample standard deviation of percolation threshold
 	 */
 	public double stddev() {
-	    stdDeviation = StdStats.stddev(openSites);
+	    double stdDeviation = StdStats.stddev(openSites);
 		return stdDeviation;
 	}
 	/*
 	 * low  endpoint of 95% confidence interval
 	 */
+	private final double CONFIDENCE_95 = 1.96;
 	public double confidenceLo() {
-	    double cl = mean() - (1.96 * stddev()) / getTrials(); 
+	    double cl = mean() - (CONFIDENCE_95 * stddev()) / getTrials(); 
 		return  cl;
 	}
 	/*
 	 * high endpoint of 95% confidence interval
 	 */
 	public double confidenceHi() {
-	    double ch = mean() + (1.96 * stddev()) / getTrials(); 
+	    double ch = mean() + (CONFIDENCE_95 * stddev()) / getTrials(); 
         return  ch;
 	}  
 	/*
@@ -119,5 +110,6 @@ public class PercolationStats {
 	    int n = 200;
 	    int trials = 100;
 	    PercolationStats ps = new PercolationStats(n, trials);
+	    log(ps);
 	}  
 }
