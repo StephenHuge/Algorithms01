d528b6dbc31657793fd7368c5ce29512869aa47d
pomony <40741004@qq.com>
2017-10-02 11:38:01 +0800

    rewrite Percolation.java

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,116 +1,114 @@
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
+import edu.princeton.cs.algs4.UF;
 /**
  * implement class Percolation 
  * 
  * @author HJS
  * 
- * @date 2017-9-29
+ * @date 2017-10-2
  * 
  */
 public class Percolation {
     
-    private boolean[][] grid;
+    private final boolean[][] sites;    // model that we use for class Percolation 
+    private final UF ufP;     // for percolates() method, there are (n * n + 2) sites
+    private final UF ufF;     // for isFull() method, there are (n * n + 1) sites
     
-    private static final boolean OPENED = true;
+    private final int virtualUpRootP;    // virtual up root for ufP 
+    private final int virtualDownRootP;  // virtual down root for ufP
     
-    private int openedNum = 0;
+    private final int virtualUpRootF;   // virtual up root for ufF, ufF has no virtual down root
     
-    private final int virtualUpRoot;
-    private final int virtualDownRoot;
+    private int openedCount;        // record count of opened sites 
     
-    private final WeightedQuickUnionUF w;
-    /*
-     * create n-by-n grid, with all sites blocked
-     */
-    public Percolation(int n) {
-        if (n <= 0) throw new java.lang.IllegalArgumentException();
+    public Percolation(int n)                
+    {
+        sites = new boolean[n][n];  // create n-by-n grid, with all sites blocked
         
-        grid = new boolean[n][n];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                grid[i][j] = false;
-            }
-        }
+        // init all variables
+        ufP = new UF(n * n + 2);
+        virtualUpRootP = n * n;
+        virtualDownRootP = n * n + 1;
         
-        w = new WeightedQuickUnionUF(n * n + 2);
-        virtualUpRoot = n * n;
-        virtualDownRoot = n * n + 1;
+        ufF = new UF(n * n + 1);
+        virtualUpRootF = n * n;
+        
+        openedCount = 0;
     }
-    /*
-     *  open site (row, col) if it is not open already
-     */
-    public void open(int row, int col) {
+    public void open(int row, int col)    // open site (row, col) if it is not open already
+    {
         validate(row, col);
-        if (!isOpen(row, col)) {
-            grid[row - 1][col - 1] = OPENED;
-            openedNum++;
+        if (!isOpen(row, col)) 
+        {
+            sites[row - 1][col - 1] = true; // open the site
+            openedCount++;
         }
-        int position = (row - 1) * grid.length + col - 1;
-        int up = (row - 2) * grid.length + col - 1;
-        int down = row * grid.length + col - 1;
-        int left = (row - 1) * grid.length + (col - 2);
-        int right = (row - 1) * grid.length + col;
-        
-        if (row == 1)             // there is a virtual root for row 1, for simplify the codes
-            w.union(position, virtualUpRoot);
-        if(row == grid.length) {
-            w.union(virtualDownRoot, position);
-        }
-        if (row != 1 && isOpen(row - 1, col))    // whether in the up edge
-            w.union(position, up);   
-        if ((row != grid.length) && isOpen(row + 1, col)) // whether in the down edge
-            w.union(position, down);
-        if (col != 1 && isOpen(row, col - 1)) // whether in the left edge             
-            w.union(position, left);
-        if ((col != grid.length) && isOpen(row, col + 1)) // whether in the right edge 
-            w.union(position, right);
+        spread(row, col);       // connect sites beside it which are opened
     }
-    /*
-     * is site (row, col) open?
-     */
-    public boolean isOpen(int row, int col) {
-       validate(row, col);
-        return grid[row - 1][col - 1];
-    } 
-    /*
-     * is site (row, col) full (means that site gets water)?
-     */
-    public boolean isFull(int row, int col) {
+    private void spread(int row, int col) 
+    {
         validate(row, col);
-        int position = (row - 1) * grid.length + col - 1;   // current site
-        // if connected with virtual up root, then return true
-        if (isOpen(row, col) && w.connected(position, virtualUpRoot)) 
-                return true;
-        return false;
-    } 
-    /*
-     *  does the system percolate?
-     */
-    public boolean percolates() {
-        return w.connected(virtualUpRoot, virtualDownRoot);
+        
+        int position = (row - 1) * sites.length + col - 1;
+        int up = (row - 2) * sites.length + col - 1;        // site above
+        int down = row * sites.length + col - 1;            // site below
+        int left = (row - 1) * sites.length + (col - 2);    // site in the left
+        int right = (row - 1) * sites.length + col;         // site in the right
+        if (row == 1) 
+        {
+            ufP.union(position, virtualUpRootP);
+            ufF.union(position, virtualUpRootF); 
+        }
+        if (row == sites.length) 
+        {
+            ufP.union(position, virtualDownRootP);
+        }
+        if (row != 1 && isOpen(row - 1, col))   // whether in the up edge 
+        { 
+            ufP.union(position, up);
+            ufF.union(position, up);
+        }           
+        if (row != sites.length && isOpen(row + 1, col))   // whether in the down edge
+        {
+            ufP.union(position, down);
+            ufF.union(position, down);
+        }
+        if (col != 1 && isOpen(row, col - 1))  // whether in the left edge
+        {
+            ufP.union(position, left);
+            ufF.union(position, left);
+        }
+        if (col != sites.length && isOpen(row, col + 1))  // whether in the left edge
+        {
+            ufP.union(position, right);
+            ufF.union(position, right);
+        }
+    }
+    public boolean isOpen(int row, int col)  // is site (row, col) open?
+    {
+        validate(row, col);
+        return sites[row - 1][col - 1];
+    }
+    public boolean isFull(int row, int col)  // is site (row, col) full?
+    {
+        validate(row, col);
+        int position = (row - 1) * sites.length + col - 1;
+        return ufF.connected(position, virtualUpRootF);
+    }
+    public int numberOfOpenSites()       // number of open sites
+    {
+        return openedCount;
+    }
+    public boolean percolates()              // does the system percolate?
+    {
+        return ufP.connected(virtualUpRootP, virtualDownRootP);
     }
     
-    private void validate(int row, int col) {
-        if (row < 1 || row > grid[0].length || col < 1 || col > grid.length) 
+    private void validate(int row, int col) 
+    {
+        if (row < 1 || row > sites.length || col < 1 || col > sites.length)
             throw new java.lang.IllegalArgumentException();
     }
-    public int numberOfOpenSites() {
-        return openedNum;
-    }     
-    /*
-     * test client (optional)
-     */
-    public static void main(String[] args) {
-        Percolation p = new Percolation(10);
-        
-        for (int i = 1; i <= p.grid.length; i++) {
-            for (int j = 1; j <= p.grid.length; j++) {
-                p.open(i, j);
-                p.isFull(i, j);
-                System.out.println(i + " - " + j + ": " + p.grid[i - 1][j - 1]);
-            }
-        }
-        System.out.println(p.numberOfOpenSites());
-    } 
+    public static void main(String[] args)   // test client (optional)
+    {
+    }
 }
